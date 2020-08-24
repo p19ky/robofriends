6a9420c0f35bb87f8ca88f84e0a3cda3a9a7b1fc
p19ky <61684270+p19ky@users.noreply.github.com>
2020-08-24 19:15:39 +0300

    last commit before deployment

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import "./App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
@@ -39,7 +40,9 @@ class App extends React.Component {
         <h1 className="f2">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
